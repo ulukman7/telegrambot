f0116a292ce59be2cb07d4b3f15cf6cc7a3b6d67
ulukman7 <ulukman091101@gmail.com>
2023-09-12 00:10:42 +0600

    added check check

--- qr.py
+++ qr.py
@@ -2,7 +2,7 @@ from pyzbar.pyzbar import decode
 from PIL import Image
 
 # Откройте изображение с QR-кодом
-image = Image.open('receipt.jpeg')
+image = Image.open('qr_1 (1).png')
 
 # Попробуйте прочитать QR-код
 try:
@@ -10,5 +10,9 @@ try:
     for obj in decoded_objects:
         data = obj.data.decode('utf-8')
         print(f"Содержимое QR-кода: {data}")
+
+        # Проверяем, содержит ли данные фразу "tax.salyk.kg"
+        if "tax.salyk.kg" in data:
+            print("Это чек")
 except Exception as e:
     print(f"Ошибка при чтении QR-кода: {e}")
