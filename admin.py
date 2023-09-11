927e721faf43fdc5d84d147d4fc17cfabc164e57
ulukman7 <ulukman091101@gmail.com>
2023-09-11 22:50:27 +0600

    first cimmit

---
+++ admin.py
@@ -0,0 +1,256 @@
+
+import sqlite3
+import random
+import re
+from datetime import datetime
+from telebot import TeleBot
+from telebot import  types
+bot = TeleBot("6582575861:AAHK19MQ8_PIkZjLIETzBizj3i0QS36eT7Q")
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    add_btn = types.KeyboardButton("➕ Добавить товар")
+    list_btn = types.KeyboardButton("📋 Список товара")
+    list_draw_btn = types.KeyboardButton("📋 Список по розыгрышу.")
+    start_btn = types.KeyboardButton("▶️ Начать розыгрыш")
+    markup.add(add_btn, list_btn, list_draw_btn, start_btn)
+    bot.send_message(message.chat.id, "Welcome to admin bot!")
+
+
+# Mahsulot qo'shish
+@bot.message_handler(commands=['add_product'])
+def add_product(message):
+    bot.send_message(message.chat.id, "Enter product name:")
+    bot.register_next_step_handler(message, add_product_name)
+
+
+
+def add_product_name(message):
+  conn = sqlite3.connect('database.db', check_same_thread=False)
+  cursor = conn.cursor()
+  name = message.text
+
+  cursor.execute("INSERT INTO products VALUES (?, ?)", (None, name))
+
+  data = cursor.fetchone()
+
+  if data is None:
+    cursor.execute("""CREATE TABLE IF NOT EXISTS products (
+                id INTEGER PRIMARY KEY,
+                name TEXT);""")
+    cursor.execute("INSERT INTO products (name) VALUES (?)", (name,))
+    bot.send_message(message.chat.id, "🎉 Товар успешно добавлен!")
+  conn.commit()
+  conn.close()
+
+  # Xabarni jo'natish
+
+# Mahsulotlar ro'yxati
+@bot.message_handler(commands=['list_products'])
+def get_products(message):
+    conn = sqlite3.connect('database.db', check_same_thread=False)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM products")
+    products = cursor.fetchall()
+
+    if len(products) == 0:
+        bot.send_message(message.chat.id, "There are no products yet")
+        return
+
+    text = "Registered products:\n"
+    for product in products:
+        text += f"{product[0]}. {product[1]}\n"
+
+    bot.send_message(message.chat.id, text)
+    conn.commit()
+    conn.close()
+
+# Auktsion uchun mahsulot tanlash
+@bot.message_handler(commands=['select_auction_product'])
+def select_auction_product(message):
+    # Avval mahsulotlar ro'yxatini ko'rsatamiz
+
+    conn = sqlite3.connect('database.db', check_same_thread=False)
+    cursor = conn.cursor()
+    cursor.execute("""
+  CREATE TABLE IF NOT EXISTS auction_products (
+    id INTEGER PRIMARY KEY,
+    product_id INTEGER
+  )
+""")
+    cursor.execute("SELECT * FROM products")
+    product_list = cursor.fetchall()
+    auction_names = ["%s. %s" % (a[0], a[1]) for a in product_list]
+    auctions_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    auctions_keyboard.add(*auction_names)
+
+    bot.send_message(message.chat.id, "Choose auction:", reply_markup=auctions_keyboard)
+@bot.message_handler(regexp='(\d+).')
+def add_delete_product(message):
+
+  product_id = message.text.split('.')[0]
+# Knopkalar
+  add_btn = types.KeyboardButton("➕ Добовить")
+  delete_btn = types.KeyboardButton("🗑 Удалить")
+
+    # Knopkalar paneli
+  buttons = types.ReplyKeyboardMarkup(resize_keyboard=True)
+  buttons.add(add_btn, delete_btn)
+
+    # Knopkalarni jo'natish
+  bot.send_message(message.chat.id, "выбирай", reply_markup=buttons)
+
+# Mahsulotni auktsionga qo'shish
+@bot.message_handler(text='➕ add')
+def add_product_to_auction(message):
+  product_id = message.text.split(' ')[1]
+
+  conn = sqlite3.connect('database.db')
+  cursor = conn.cursor()
+  cursor.execute("""
+  CREATE TABLE IF NOT EXISTS auction_products (
+    id INTEGER PRIMARY KEY,
+    product_id INTEGER
+  )
+""")
+  cursor.execute("SELECT * FROM products WHERE id=?", (product_id,))
+  product = cursor.fetchone()
+
+  if product:
+    cursor.execute("INSERT INTO auction_products (product_id) VALUES (?)", (product_id,))
+    conn.commit()
+    bot.reply_to(message, "Товар успешно добавлен")
+  else:
+    bot.reply_to(message, "Такой товар не найден")
+
+  conn.close()
+
+# Mahsulotni auktsiondan olib tashlash
+@bot.message_handler(commands=['delete_product_from_auction'])
+def delete_product_from_auction(message):
+  product_id = message.text.split(' ')[1]
+
+  conn = sqlite3.connect('database.db')
+  cursor = conn.cursor()
+
+  cursor.execute("DELETE FROM auction_products WHERE product_id=?", (product_id,))
+  conn.commit()
+
+  bot.reply_to(message, "Продукт успешно удален")
+
+  conn.close()
+
+# Knopkalar uchun handlerlar
+@bot.message_handler(text="➕ Добавить")
+def add(message):
+    product_id = message.text # bu yerga mahsulot ID si kiritiladi
+    add_product_to_auction(product_id)
+
+@bot.message_handler(text="🗑 Удалить")
+def delete(message):
+    product_id = message.text
+    delete_product_from_auction(product_id)
+
+@bot.message_handler(commands=['delete_product'])
+def delete_product_by_id(message):
+  conn = sqlite3.connect('database.db', check_same_thread=False)
+  cursor = conn.cursor()
+  cursor.execute("""
+  CREATE TABLE IF NOT EXISTS auction_products (
+    id INTEGER PRIMARY KEY,
+    product_id INTEGER
+  )
+""")
+  product_id = message.text
+
+  cursor.execute("DELETE FROM products WHERE id=?", (product_id,))
+  conn.commit()
+  conn.close()
+  bot.send_message(message.chat.id, "Product deleted successfully")
+# акция
+@bot.message_handler(commands=['create_auction'])
+def create_auction(message):
+    bot.send_message(message.chat.id, "Enter auction name:")
+    bot.register_next_step_handler(message, auction_name_step)
+
+def auction_name_step(message):
+    name = message.text
+    # Оставшиеся шаги...
+    bot.send_message(message.chat.id, "Enter start date (DD.MM.YYYY):")
+    bot.register_next_step_handler(message, auction_start_date_step, name)
+
+def auction_start_date_step(message, name):
+    start_date = message.text
+    #...
+    bot.send_message(message.chat.id, "Enter end date (DD.MM.YYYY):")
+    bot.register_next_step_handler(message, auction_end_date_step, name, start_date)
+
+def auction_end_date_step(message, name, start_date):
+   end_date = message.text
+   #...
+   bot.send_message(message.chat.id, "Enter minimum amount:")
+   bot.register_next_step_handler(message, auction_min_amount_step, name, start_date, end_date)
+
+def auction_min_amount_step(message, name, start_date, end_date):
+
+    conn = sqlite3.connect('database.db', check_same_thread=False)
+    cursor = conn.cursor()
+    cursor.execute("""
+CREATE TABLE IF NOT EXISTS auctions (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    start_date TEXT,
+    end_date TEXT,
+    min_amount INTEGER
+);
+""")
+    min_amount = message.text
+
+# Мы сохраняем данные аукциона
+    cursor.execute("INSERT INTO auctions VALUES (?, ?, ?, ?, ?)",
+                   (None, name, start_date, end_date, min_amount))
+
+    bot.send_message(message.chat.id, "Auction created successfully!")
+
+    conn.commit()
+    conn.close()
+# Список участников
+@bot.message_handler(commands=['auction_participants'])
+def auction_participants(message):
+# Сначала показываем список аукционов
+    conn = sqlite3.connect('database.db', check_same_thread=False)
+    cursor = conn.cursor()
+    cursor.execute("""
+CREATE TABLE IF NOT EXISTS participants (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    phone TEXT,
+    receipt_id INTEGER,
+    auction_id INTEGER
+);
+""")
+    cursor.execute("SELECT * FROM auctions")
+    auctions = cursor.fetchall()
+
+    auction_names = ["%s. %s" % (a[0], a[1]) for a in auctions]
+    auctions_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    auctions_keyboard.add(*auction_names)
+
+    bot.send_message(message.chat.id, "Choose auction:", reply_markup=auctions_keyboard)
+
+    # bot.register_next_step_handler(message, show_participants)
+
+# def show_participants(message):
+#     auction_name = message.text
+    # Tanlangan auktsion bo'yicha ishtirokchilar ro'yxatini ko'rsatish
+    # Har bir uchun ism, tel, chek raqami ko'rsatiladi
+
+# G'olibni tanlash
+# @bot.message_handler(commands=['select_winner'])
+# def select_winner(auction_id):
+#   winner_index = random.randint(0, len(participants)-1)
+#   winner = participants[winner_index]
+#   bot.send_message(chat_id, f"Winner: {winner.name} ({winner.phone})")
+bot.polling()
---
+++ user.py
@@ -0,0 +1,99 @@
+import telebot
+from telebot import types
+import sqlite3
+import re
+import uuid
+bot = telebot.TeleBot("6339660614:AAHOAnVWlNrIs8JLSn8cGc_TAzRMCAKMju4")
+
+cheks_button = types.InlineKeyboardButton(text="Cheklarim", callback_data="get_cheks")
+keyboard = types.InlineKeyboardMarkup()
+keyboard.add(cheks_button)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Ваше Имя:")
+    bot.register_next_step_handler(message, get_name)
+
+def get_name(message):
+    user_name = message.text
+    bot.send_message(message.chat.id, "Ваше Фамилия:")
+    bot.register_next_step_handler(message, get_lastname, user_name)
+
+def get_lastname(message, user_name):
+  user_lastname = message.text
+
+  bot.send_message(message.chat.id, "Ваш номер телефона в международном формате (+996*********):")
+  bot.register_next_step_handler(message, get_phone, user_name, user_lastname)
+
+def get_phone(message, user_name, user_lastname):
+  user_phone = message.text
+  if not re.match("\+\d{3}\d{8}", user_phone):
+    bot.send_message(message.chat.id, "Номер не в международном формате")
+    return
+  # Ma'lumotlarni saqlash
+  add_user(user_name, user_lastname, user_phone, message.chat.id)
+  bot.send_message(message.chat.id, "✅ Вы успешно зарегистрированы!")
+  bot.send_message(message.chat.id, "📃 Отправьте фото чека")
+def add_user(name, lastname, phone, chat_id):
+
+  conn = sqlite3.connect('database.db')
+  c = conn.cursor()
+
+  # Foydalanuvchi mavjudligini tekshirish
+  c.execute("SELECT * FROM users WHERE chat_id = ?", (chat_id,))
+  data = c.fetchone()
+  if data is None:
+    # Foydalanuvchi yo'q, qo'shamiz
+    conn.execute('''CREATE TABLE IF NOT EXISTS users 
+               (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                name TEXT, 
+                phone TEXT)''')
+    c.execute("INSERT INTO users VALUES (?, ?, ?, ?)",
+              (name, lastname, phone, chat_id))
+  else:
+    # Foydalanuvchi bor, xabardor qilamiz
+    bot.send_message(chat_id, "Вы уже зарегистрированы")
+
+  conn.commit()
+  conn.close()
+
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+  file_id = message.photo[-1].file_id
+  img_id = str(uuid.uuid4())
+  save_receipt(img_id, file_id, message.chat.id)
+
+  bot.send_message(message.chat.id, "✅ Чек успешно получен!")
+
+def save_receipt(img_id, file_id, chat_id):
+
+  conn = sqlite3.connect('database.db')
+
+  conn.execute('''CREATE TABLE IF NOT EXISTS receipts
+             (id text, file_id text, chat_id text)''')
+
+  conn.execute("INSERT INTO receipts VALUES (?, ?, ?)",
+            (img_id, file_id, chat_id))
+
+  conn.commit()
+  conn.close()
+def get_user_cheks(chat_id):
+  conn = sqlite3.connect('database.db')
+  c = conn.cursor()
+  c.execute("SELECT * FROM receipts WHERE chat_id=?", (chat_id,))
+  cheks = c.fetchall()
+
+  return cheks
+
+@bot.message_handler(commands=['chek'])
+def get_cheks(msg):
+
+  chat_id = msg.chat.id
+  cheks = get_user_cheks(chat_id)
+
+  for chek in cheks:
+    file_id = chek[1]
+    bot.send_photo(chat_id, file_id)
+
+
+bot.polling()
