d670ce4597802fd493066026addd8cc104b9d7ab
ikboljon1 <ikboljonuldashvaev@gmail.com>
2023-09-11 13:47:04 -0400

    prize

--- ocr.py
+++ ocr.py
@@ -1,7 +1,7 @@
 import json
 import requests
 url = "https://ocr.asprise.com/api/v1/receipt"
-image = "receipt.jpeg"
+image = "receipt3.jpg"
 res = requests.post(url,
                     data = {
                         'api_key': 'TEST',
@@ -11,5 +11,5 @@ res = requests.post(url,
                     files = {
                         'file': open(image, 'rb')
                     })
-with open("response1.json", "w") as f:
+with open("response3.json", "w") as f:
     json.dump(json.loads(res.text), f)
--- ocr1.py
+++ ocr1.py
@@ -1,13 +1,11 @@
 import requests
 import json
 
-with open("response1.json", "r") as f:
+with open("response3.json", "r") as f:
     data = json.load(f)
 print(data['receipts'][0].keys())
 
 items = data['receipts'][0]['items']
 
-print(f'Your Purchase at {data["receipts"][0]["merchant_name"]}')
+print(f'Your Purchase at {data["receipts"][0]["ocr_text"]}')
 
-for item in items:
-    print(f"{item['description']} - {item['amount']}")
